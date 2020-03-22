bca32af82ef6dd3cf27dc616f18844e7a3f30a27
WondeTade <won_tade@yahoo.com>
2020-03-22 21:45:56 +0100

    cross origin enabled

--- src/main/java/com/wonde/celebritiesofficialpageinfoservice/controller/PersonsInfoController.java
+++ src/main/java/com/wonde/celebritiesofficialpageinfoservice/controller/PersonsInfoController.java
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/person/")
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 public class PersonsInfoController {
 
     private PersonInfoRepository personInfoRepository;
